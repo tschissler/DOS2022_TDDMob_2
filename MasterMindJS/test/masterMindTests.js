3c6db0974e669ce353489fbec58f8f703d815a08
tschissler <tschissler@agilemax.de>
2022-12-02 08:26:47 +0100

    Removed unused folder

--- MasterMindJS/test/masterMindTests.js
+++
@@ -1,8 +0,0 @@
-const assert=require('assert').strict;
-const masterMindLogic = require('../masterMindLogic.js');
-
-describe('GetGuessFeedbackTests', function() {
-  it('TestCaseName', function() {
-    assert.deepStrictEqual(1, 2);
-  });
-});
